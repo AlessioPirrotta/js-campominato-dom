46171fd3153c00ef2d9bdfcb3839313a1a13cb39
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-01-18 01:42:30 +0100

    exercise completed

--- main.js
+++ main.js
@@ -15,154 +15,155 @@
 const griglia = document.getElementById("grid");
 const button = document.getElementById("btn");
 const difficoltà = document.getElementById("sct");
-let arrayBombeE= []
-let arrayBombeM= []
-let arrayBombeH=[]
-let arrayBox=[]
+let risultato = document.getElementById("rs");
+let punteggio = document.getElementById("pt");
+let arrayBombeE = [];
+let arrayBombeM = [];
+let arrayBombeH = [];
+let arrayBox = [];
 const difficoltaIniziale = difficoltà.value;
-let gameOver= false 
-let checkbox= 0
-let safebox= 0
+let gameOver = false;
+let checkbox = 0;
+let safebox = 0;
 
-
-for(let j=0;j< 16; j++){
-     bomba = generaNumeroCasuale(1, 100)
-     if (!arrayBombeE.includes(bomba)) {
-         arrayBombeE.push(bomba);
-        }
+for (let j = 0; j < 16; j++) {
+    bomba = generaNumeroCasuale(1, 100);
+    if (!arrayBombeE.includes(bomba)) {
+        arrayBombeE.push(bomba);
     }
-for(let j=0;j< 16; j++){
-    bomba = generaNumeroCasuale(1, 81)
+}
+
+for (let j = 0; j < 16; j++) {
+    bomba = generaNumeroCasuale(1, 81);
     if (!arrayBombeM.includes(bomba)) {
         arrayBombeM.push(bomba);
-        }
     }
-    for(let j=0;j< 16; j++){
-    bomba = generaNumeroCasuale(1, 49)
+}
+
+for (let j = 0; j < 16; j++) {
+    bomba = generaNumeroCasuale(1, 49);
     if (!arrayBombeH.includes(bomba)) {
         arrayBombeH.push(bomba);
-        }
     }
-    
+}
 
-    button.addEventListener("click", function () {
-        griglia.classList.remove("d-none");
-    
-        const difficoltaSelezionata = difficoltà.value;
-    
-        creaGriglia(difficoltaSelezionata);
-        gameOver=false
-    });
-    
+button.addEventListener("click", function () {
+    griglia.classList.remove("d-none");
 
+    const difficoltaSelezionata = difficoltà.value;
+
+    creaGriglia(difficoltaSelezionata);
+    gameOver = false;
+    checkbox = 0;
+    safebox = 0;
+});
 
 function creaGriglia(difficolta) {
-    
+
     griglia.innerHTML = "";
 
     if (difficolta === "easy") {
-        for (let i = 0; i < 100 ; i++) {
+        for (let i = 0; i < 100; i++) {
             let box = document.createElement("div");
             box.classList.add("box");
             griglia.append(box);
-            box.innerHTML = i + 1
+            box.innerHTML = i + 1;
+
             box.addEventListener("click", function () {
+                if (gameOver === true) {
+                    return;
+                }
 
-                safebox= 100 - arrayBombeE
-
-                if (gameOver=== true) {
-                    return; 
-                  }
                 this.classList.toggle("active");
                 console.log(this);
+
                 if (arrayBombeE.includes(parseInt(box.innerHTML))) {
-                    box.classList.toggle("activebomb")
-                    gameOver= true
-                  }
-                else {
+                    box.classList.toggle("activebomb");
+                    gameOver = true;
+                    risultato.innerHTML= "Hai perso"
+                    punteggio.innerHTML= `Punteggio ${checkbox}`
+                } else {
                     checkbox++;
                 }
 
-                if(safebox === checkbox){
-                    gameOver=true
+                if (checkbox === 100 - arrayBombeE.length) {
+                    gameOver = true;
+                    risultato.innerHTML= "Hai vinto"
                 }
-
-                
-
-                 
             });
-            
-            arrayBox.push(box)
-            
+
+            arrayBox.push(box);
         }
-        
-        
+
     } else if (difficolta === "medium") {
         for (let i = 0; i < 81; i++) {
             let box = document.createElement("div");
             box.classList.add("box2");
             griglia.append(box);
             box.innerHTML = i + 1;
+
             box.addEventListener("click", function () {
-                safebox= 81 - arrayBombeM
-                if (gameOver=== true) {
-                    return; 
-                  }
+                if (gameOver === true) {
+                    return;
+                }
+
                 this.classList.toggle("active");
                 console.log(this);
+
                 if (arrayBombeM.includes(parseInt(box.innerHTML))) {
                     box.classList.toggle("activebomb");
-                    gameOver= true
-                  }
-                  else {
+                    gameOver = true;
+                } else {
                     checkbox++;
                 }
 
-                if(safebox === checkbox){
-                    gameOver=true
+                if (checkbox === 81 - arrayBombeM.length) {
+                    gameOver = true;
                 }
             });
-            arrayBox.push(box)
+
+            arrayBox.push(box);
         }
-        console.log(arrayBox)
-    }
-    else if (difficolta === "hard") {
+    } else if (difficolta === "hard") {
         for (let i = 0; i < 49; i++) {
             let box = document.createElement("div");
             box.classList.add("box3");
             griglia.append(box);
             box.innerHTML = i + 1;
+
             box.addEventListener("click", function () {
-                safebox= 49 - arrayBombeH
-                if (gameOver=== true) {
-                    return; 
-                  }
+                if (gameOver === true) {
+                    return;
+                }
+
                 this.classList.toggle("active");
                 console.log(this);
+
                 if (arrayBombeH.includes(parseInt(box.innerHTML))) {
                     box.classList.toggle("activebomb");
-                    gameOver= true
-                  }
-                  else {
+                    gameOver = true;
+                } else {
                     checkbox++;
                 }
 
-                if(safebox === checkbox){
-                    gameOver=true
-                }  
+                if (checkbox === 49 - arrayBombeH.length) {
+                    gameOver = true;
+                }
             });
+
+            arrayBox.push(box);
         }
     }
 }
 
-
-console.log(arrayBombeH)
-
-
+console.log(arrayBombeH);
 
 function generaNumeroCasuale(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
+
+
+//   limite di tentativi e messaggio finale 
 
 
 
